a128b1ffdf56baf70abf347c71abd8e94dda1187
Graden <101433010+GramGra07@users.noreply.github.com>
2024-04-04 22:50:51 -0600

    fixed and uploaded

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareConfig.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareConfig.kt
@@ -1,14 +1,11 @@
 //import
 package org.firstinspires.ftc.teamcode
 
-import com.qualcomm.hardware.lynx.LynxModule
 import com.qualcomm.hardware.rev.RevBlinkinLedDriver
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import com.qualcomm.robotcore.hardware.HardwareMap
-import com.qualcomm.robotcore.hardware.VoltageSensor
 import org.firstinspires.ftc.teamcode.extensions.BlinkExtensions.initLights
 import org.firstinspires.ftc.teamcode.extensions.BlinkExtensions.setPatternCo
-import org.firstinspires.ftc.teamcode.extensions.SensorExtensions.initVSensor
 
 
 open class HardwareConfig() {
@@ -20,21 +17,13 @@ open class HardwareConfig() {
 
     companion object {
         lateinit var lights: RevBlinkinLedDriver
-        lateinit var vSensor: VoltageSensor
         private lateinit var myOpMode: LinearOpMode
-
-        var allHubs: List<LynxModule> = ArrayList()
     }
 
     private fun initRobot(
         ahwMap: HardwareMap,
     ) {
-        vSensor = initVSensor(ahwMap, "Expansion Hub 2")
         lights = initLights(ahwMap, "blinkin")
-        allHubs = ahwMap.getAll(LynxModule::class.java)
-        for (hub in allHubs) {
-            hub.bulkCachingMode = LynxModule.BulkCachingMode.MANUAL
-        }
         lights.setPatternCo(RevBlinkinLedDriver.BlinkinPattern.COLOR_WAVES_RAINBOW_PALETTE)
     }
 
