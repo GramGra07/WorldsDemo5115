bd4151c5519897082784f05ad307b546f79646d9
Cgsmith24 <cgsmith24@weldre4.org>
2024-04-16 13:54:23 -0600

    Fix vital issue

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareConfig.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareConfig.kt
@@ -29,9 +29,9 @@ open class HardwareConfig() {
 
     fun doBulk() {
         if (myOpMode.gamepad1.cross) {
-            lights.setPatternCo(RevBlinkinLedDriver.BlinkinPattern.COLOR_WAVES_LAVA_PALETTE)
+            lights.setPatternCo(RevBlinkinLedDriver.BlinkinPattern.ORANGE)
         } else if (myOpMode.gamepad1.circle) {
-            lights.setPatternCo(RevBlinkinLedDriver.BlinkinPattern.COLOR_WAVES_FOREST_PALETTE)
+            lights.setPatternCo(RevBlinkinLedDriver.BlinkinPattern.CONFETTI)
         }
     }
 }
